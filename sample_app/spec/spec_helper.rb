5ba4026097c7b3e7e07c3d90ee0e1a835bfff9fb
Alain Garcia <alan.puas2001@gmail.com>
2012-05-23 16:30:03 -0500

    rspec

--- sample_app/spec/spec_helper.rb
+++ sample_app/spec/spec_helper.rb
@@ -2,6 +2,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'rubygems'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -25,3 +26,4 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 end
+
